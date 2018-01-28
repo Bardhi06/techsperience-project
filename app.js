25b82a9ca767d5f90341a68a5228ef2120c0da0b
unknown <fatbardh8991@gmail.com>
2018-01-28 13:30:34 +0100

    update, delete and add

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 // init app
 const app = express();
 
@@ -37,6 +36,70 @@ MongoClient.connect(mongoURL, function(err, db) {
 //routes
 app.get('/', function(req, res) {
   res.render("index");
+});
+
+app.get('/products', function(req, res) {
+
+     Product.find(function (err, products) {
+       if (err) {
+         console.log(err); }
+         res.json(products);
+     });
+  });
+
+
+app.post('/products', function(req, res) {
+
+   // Code for handling create product to DB and return as JSON
+
+   var product = new Product();
+   product.product_name = req.body.product_name;
+   product.product_description = req.body.product_description;
+   product.product_price = req.body.product_price;
+
+   Product.create(product, function (err, product){
+     if (err) { console.log(err); }
+     res.json(product);
+   });
+});
+
+
+app.get('/products/:id', function(req, res) {
+
+   // Code for getting one specific product and return as JSON
+
+   Product.findById(req.params.id, function(err, product){
+     if (err) {
+       console.log(err);
+     }
+     res.json(product);
+   });
+});
+
+
+app.put('/products/update/:id', function(req, res) {
+   // Code for updating one specific product and return as JSON
+   var updatedProduct = {
+     product_name: req.body.product_name,
+     product_description: req.body.product_description,
+     product_price: req.body.product_price
+   }
+   // setting {new: true} e kthen meniher tUpdated
+   Product.findOneAndUpdate({_id: req.params.id}, updatedProduct, {new: true}, function(err, product){
+     if (err) {
+      console.log(err);
+     }
+     res.json(product);
+   });
+});
+
+/* DELETE PRODUCT */
+app.delete('/products/delete/:id', function(req, res) {
+   // Code to delete one specific product and return message as JSON
+   Product.remove({_id: req.params.id}, function (err, result) {
+     if (err){ console.log(err)};
+     res.json({message: " Product Successfully deleted! "});
+   });
 });
 
 app.listen(3010, function() {
